496edb540a95d9eb294aec14c95d5fe51059e750
panda <3376694448@qq.com>
2022-07-29 09:40:33 +0800

    源码流程梳理及六种配置示例

--- README.md
+++ README.md
@@ -3,6 +3,25 @@
 
 [源码-v3.5.2](https://github.com/baomidou/generator)
 
-# init
+## init
 
 源码是一个单体项目`mybatis-plus-generator`,是基于gradle进行打包的，参考build.gradle新建pom文件；同时更新readme。
+
+## 工作流程
+
+根据**用户配置**从数据库中**查询表、字段信息**存入**模板引擎上下文信息**
+
+- **用户配置**  基于构建者模式、消费者模式将六种配置统一到`ConfigBuilder`：<br>
+`DataSourceConfig 数据源配置` `GlobalConfig 全局配置` `PackageConfig 包配置` `InjectionConfig 注入配置` `TemplateConfig 模板配置` `StrategyConfig 库表配置` 
+- **查询表、字段信息**  `ConfigBuilder`提供接口查询数据表、字段信息；目前有元数据查询、SQL查询两种实现。
+- **模板引擎上下文信息**  存放在一个Map中，注入配置提供了设置自定义上下文信息的接口和Consumer函数
+
+主流程相关函数  `AutoGenerator.execute()` `AbstractTemplateEngine.batchOutput()`
+
+`ConfigBuilder`查询接口实现  元数据查询`DefaultQuery.queryTables()` `DefaultQuery.convertTableFields()`
+
+SQL查询`SQLQuery.queryTables()` `SQLQuery.convertTableFields()`
+
+## 配置信息
+
+参考[官方文档](https://baomidou.com)及代码示例
---
+++ mybatis-plus-generator/src/main/java/com/baomidou/mybatisplus/generator/MyGenerator.java
@@ -0,0 +1,110 @@
+package com.baomidou.mybatisplus.generator;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.po.TableField;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+
+import java.io.File;
+import java.util.Collections;
+
+public class MyGenerator {
+    private static final String url = "jdbc:mysql://192.168.1.109:3306/sclead_tpm" +
+        "?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull" +
+        "&useSSL=false&serverTimezone=GMT%2B8&allowMultiQueries=true";;
+    private static final String username = "root";
+    private static final String password = "123456";
+    private static String outputDir = "D:\\idea_workplace\\generator\\mybatis-plus-generator\\src\\main\\java";
+    private static String parent = "com.skkj.tpm.config";
+    private static final String tableName = "tb_equipment_check_group";
+
+    public static void main(String[] args) {
+        generator();
+    }
+
+    private static void generator() {
+        /* 数据源配置 */
+        DataSourceConfig.Builder dataSourceConfig = new DataSourceConfig.Builder(url, username, password)
+//            .dbQuery(new MySqlQuery())  //是否使用sql查询表、字段信息
+        ;
+
+        FastAutoGenerator.create(dataSourceConfig)
+            /* 全局配置 */
+            .globalConfig(builder -> builder
+                .disableOpenDir()   //禁止打开输出目录
+                .outputDir(outputDir)   //指定输出目录
+                .author("xjx")  //作者
+                .enableSwagger()
+                .dateType(DateType.TIME_PACK)
+                .commentDate("yyyy-MM-dd")
+            )
+            /* 包配置 */
+            .packageConfig(builder -> builder
+                .parent(parent) //父包名
+//                .moduleName(parent) //父包模块名
+                .entity("domain")   //entity 包名
+                .pathInfo(Collections.singletonMap(OutputFile.xml
+                    ,outputDir.replace("java","resources\\mapper")))   //替换默认 xml 路径
+            )
+            /* 模板配置 */
+//            .templateConfig(builder -> builder
+//                .controller("/templates/sclead/itip-api/controller.java")
+//                .service("/templates/sclead/itip-api/service.java")
+//                .serviceImpl("/templates/sclead/itip-api/serviceImpl.java")
+//                .entity("/templates/sclead/itip-api/entity.java")
+//                .mapper("/templates/sclead/itip-api/mapper.java")
+//                .xml("/templates/sclead/itip-api/mapper.xml")
+//            )
+            /* 注入配置 */
+            .injectionConfig(builder -> builder
+                .beforeOutputFile((tableInfo, stringObjectMap) ->
+                    tableInfo.getFields().stream().filter(TableField::isKeyFlag).limit(1).forEach(e->{
+                    stringObjectMap.put("pkColumn",e.getPropertyName());
+                    stringObjectMap.put("pkType",e.getPropertyType());
+                })) //输出前操作，这里添加了模板上下文信息
+                .customFile(myBuilder -> {
+                    if (!StringUtils.endsWith(outputDir, File.separator)) {
+                        outputDir += File.separator;
+                    }
+                    parent = parent.replaceAll("\\.", StringPool.BACK_SLASH + File.separator);
+                    myBuilder
+                        .templatePath("/templates/sclead/itip-api/vo.java.vm" ) //模板路径
+                        .filePath(outputDir + parent +  File.separator + "vo" ) //文件路径
+                        .fileName("Vo" + StringPool.DOT_JAVA)  //源码以 entityName 开头，需和模板类名保持一致
+                        .enableFileOverride();
+                }) //自定义 Dto Vo
+            )
+            /* 策略配置 */
+            .strategyConfig(builder -> builder
+                .addInclude(tableName)  //表匹配
+                .addTablePrefix("tb_")  //过滤表前缀
+                /* 实体策略配置 */
+                .entityBuilder()
+//                .superClass(BaseEntity.class)   //设置父类
+                .enableFileOverride() //允许覆盖
+                .enableLombok() //开启 lombok
+                .enableTableFieldAnnotation()   //开启字段注释
+                .naming(NamingStrategy.underline_to_camel)  //数据库表映射到实体的命名规则,字段映射规则未配置也用这个
+                .addSuperEntityColumns("searchValue", "create_by", "create_time",
+                    "update_by", "update_time","remark","order_num")  //父类公共字段
+                /* controller 策略配置 */
+                .controllerBuilder()
+                .enableFileOverride()
+//                .superClass(BaseController.class)   //设置父类
+                .enableRestStyle()  //开启 @RestController
+                /* service 策略配置 */
+                .serviceBuilder()
+                .enableFileOverride()
+                /* mapper 策略配置 */
+                .mapperBuilder()
+                .enableFileOverride()
+            )
+            /* 模板引擎 */
+            .templateEngine(new VelocityTemplateEngine())
+            .execute();
+    }
+}
